8ad4b08ae03f8f7c61c3c25f80581a6fca6dc214
Pierre Bertrand <funkygono@gmail.com>
2013-05-03 21:07:55 +0200

    Use constructor injection.

--- src/main/java/org/funky/repository/lucene/document/DocumentBuilderImpl.java
+++ src/main/java/org/funky/repository/lucene/document/DocumentBuilderImpl.java
@@ -6,12 +6,18 @@ import org.funky.repository.lucene.DocumentBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.List;
 
 @Component
 public class DocumentBuilderImpl implements DocumentBuilder {
 
-    @Autowired List<FieldBuilder> fieldBuilders;
+    private final List<FieldBuilder> fieldBuilders;
+
+    @Autowired
+    public DocumentBuilderImpl(List<FieldBuilder> fieldBuilders) {
+        this.fieldBuilders = Collections.unmodifiableList(fieldBuilders);
+    }
 
     @Override
     public Document build(Content content) {
